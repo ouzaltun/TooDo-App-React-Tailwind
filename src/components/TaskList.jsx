2601d8f187f790ec93b75ad5a3e9cad0c166f18d
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-11-15 00:34:28 +0300

    Tasklist transform translate animate
    Tasklist transform translate animate

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,13 +2,11 @@ import React from "react";
 function SignInForm({ tasks32 }) {
   console.log(tasks32);
   return (
-    <div className="absolute top-0 h-full w-full flex flex-col justify-center items-center tasklist-container">
-      <h1 className="font-bold absolute top-0 mt-10 text-xl">
-        Oluşturulan Tasklar
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2  px-4 gap-y-4 w-full  gap-x-2 justify-center items-center ">
+    <div className="m-0 bottom-0  flex flex-col justify-center items-center !translate-x-0 sasa  overlay-left  overlay-panel">
+      <h1 className="font-bold  top-0 mt-10 text-xl">Oluşturulan Tasklar</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 py-4 h-full   w-full  gap-x-2 justify-center items-start ">
         {tasks32.map((e) => (
-          <div>sa</div>
+          <div className="px-6 py-4">sa</div>
         ))}
       </div>
     </div>
