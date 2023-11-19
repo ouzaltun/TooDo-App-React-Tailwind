f2c04e760f14d8b2624d5471a1ab2422e5d3d0d8
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-11-19 21:25:40 +0300

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,9 +3,9 @@ import TaskItem from "./TaskItem";
 function SignInForm({ tasks32 }) {
   console.log(tasks32);
   return (
-    <div className="!absolute m-0 bottom-0 !h-[470px] overflow-y-scroll z-50 transition-none flex flex-col pointer-events-auto !justify-start items-start !translate-x-0 sasa overlay-left overlay-panel">
+    <div className="!absolute m-0 bottom-0 !h-[90%] overflow-y-scroll z-50 transition-none flex flex-col pointer-events-auto !justify-start items-start !translate-x-0 sasa overlay-left overlay-panel">
       <h1 className="font-bold top-0 mt-6 text-2xl">Oluşturulan Tasklar</h1>
-      <div className="grid grid-cols-1 w-full overflow-x-hidden md:grid-cols-1 my-4 gap-y-2 justify-start items-start pb-20">
+      <div className="grid grid-cols-1 w-full overflow-x-hidden md:grid-cols-1 my-4 gap-y-2 justify-start items-start pb-0">
         {tasks32.map((e) => {
           return <TaskItem key={e.id} task67={e}></TaskItem>;
         })}
