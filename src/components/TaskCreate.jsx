820f4927e3575b758d67eea86736ab8431838cec
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-11-22 23:19:37 +0300

    Responsive İnput text size
    İnput text size

--- src/components/TaskCreate.jsx
+++ src/components/TaskCreate.jsx
@@ -33,13 +33,13 @@ function SignUpForm({ onCreate }) {
           value={title}
           onChange={handleChange}
           placeholder="Task Başlığı"
-          className="bg-[#eee] border border-[#eabf9f] py-3 px-4 my-2 w-full rounded-3xl placeholder:text-[10px] md:placeholder:text-sm "
+          className="bg-[#eee] border border-[#eabf9f] py-3 px-4 my-2 w-full text-[10px]  md:text-[16px]  rounded-3xl placeholder:text-[10px] md:placeholder:text-sm "
         />
         <textarea
           value={taskDesc}
           onChange={handleTaskChange}
           placeholder="Task Açıklaması"
-          className="bg-[#eee] border border-[#eabf9f] py-6 h-[150px] rounded-3xl px-4 my-2 w-full placeholder:text-[10px] md:placeholder:text-sm "
+          className="bg-[#eee] border border-[#eabf9f] py-6 h-[150px] rounded-3xl text-[10px]  md:text-[16px] px-4 my-2 w-full placeholder:text-[10px] md:placeholder:text-sm "
         />
 
         <button className=" active:scale-95 rounded-3xl border border-solid-custom-main bg-custom-main text-custom-text text-[8px]  md:text-[12px] font-bold uppercase transition-transform tracking-wide py-3 px-6 md:px-11">
